2bb6565a32ec9c62e006968fa352ac4d2e92e93b
srcontrerash <scontrerash@gmail.com>
2021-06-06 21:42:43 +0000

    First Commit 04

--- Employees/webapp/controller/MainView.controller.js
+++ Employees/webapp/controller/MainView.controller.js
@@ -1,15 +1,32 @@
+// @ts-nocheck
+
 sap.ui.define([
-		"sap/ui/core/mvc/Controller"
-	],
+    "sap/ui/core/mvc/Controller"    
+],
 	/**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-	function (Controller) {
-		"use strict";
+    function (Controller) {
+        "use strict";
 
-		return Controller.extend("logaligroup.Employees.controller.MainView", {
-			onInit: function () {
+        return Controller.extend("logaligroup.Employees.controller.MainView", {
+            onInit: function () {
 
-			}
-		});
-	});
+            },
+
+            onValidate: function () {
+                var inputEmployee = this.byId("inputEmployee");
+                var valueEmployee = inputEmployee.getValue();
+
+                if (valueEmployee.length === 6) {
+                   // inputEmployee.setDescription("OK");
+                   this.byId("labelCountry").setVisible(true);
+                   this.byId("slCountry").setVisible(true);
+                } else {
+                    //inputEmployee.setDescription("NO OK");
+                    this.byId("labelCountry").setVisible(false);
+                   this.byId("slCountry").setVisible(false);
+                };
+            }
+        });
+    });
